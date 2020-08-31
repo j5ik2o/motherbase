c197b3450b82c57ef54828f314a6ae107f46b18b
Junichi Kato <j5ik2o@gmail.com>
2020-08-31 12:56:02 +0900

    wip

--- backend/modules/accounts/command/accounts-command-interface-adaptor/src/main/scala/com/github/j5ik2o/motherbase/accounts/interfaceAdaptor/http/controller/AccountCommandControllerImpl.scala
+++ backend/modules/accounts/command/accounts-command-interface-adaptor/src/main/scala/com/github/j5ik2o/motherbase/accounts/interfaceAdaptor/http/controller/AccountCommandControllerImpl.scala
@@ -5,6 +5,7 @@ import akka.http.scaladsl.server.Route
 import akka.stream.scaladsl.{ Sink, Source }
 import com.github.j5ik2o.motherbase.accounts.interfaceAdaptor.http.directives.AccountValidateDirectives
 import com.github.j5ik2o.motherbase.accounts.interfaceAdaptor.http.directives.AccountValidateDirectives._
+import com.github.j5ik2o.motherbase.accounts.interfaceAdaptor.http.exceptions.ExceptionHandlers
 import com.github.j5ik2o.motherbase.accounts.interfaceAdaptor.http.json.CreateAccountRequestJson
 import com.github.j5ik2o.motherbase.accounts.interfaceAdaptor.http.rejections.RejectionHandlers
 import com.github.j5ik2o.motherbase.accounts.interfaceAdaptor.http.responder.CreateAccountJsonResponder
@@ -18,11 +19,14 @@ final class AccountCommandControllerImpl(
 ) extends AccountCommandController
     with AccountValidateDirectives {
 
-  override def toRoutes: Route = handleRejections(RejectionHandlers.default) {
-    pathPrefix("v1") {
-      createAccount
+  override def toRoutes: Route =
+    handleExceptions(ExceptionHandlers.default) {
+      handleRejections(RejectionHandlers.default) {
+        pathPrefix("v1") {
+          createAccount
+        }
+      }
     }
-  }
 
   override private[controller] def createAccount =
     path("accounts" / "create") {
---
+++ backend/modules/accounts/command/accounts-command-interface-adaptor/src/main/scala/com/github/j5ik2o/motherbase/accounts/interfaceAdaptor/http/exceptions/ExceptionHandlers.scala
@@ -0,0 +1,17 @@
+package com.github.j5ik2o.motherbase.accounts.interfaceAdaptor.http.exceptions
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives.complete
+import akka.http.scaladsl.server.ExceptionHandler
+import com.github.j5ik2o.motherbase.accounts.interfaceAdaptor.http.json.ErrorsResponseJson
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import io.circe.generic.auto._
+
+object ExceptionHandlers {
+
+  final val default = ExceptionHandler{
+    case ex: Exception =>
+      complete((StatusCodes.InternalServerError, ErrorsResponseJson(Seq(ex.getMessage))))
+  }
+
+}
